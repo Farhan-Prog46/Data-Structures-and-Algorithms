234152c16aa953decc0926508b57b7997c7382b7
Farhan Zaheer <farhanzaheer686@gmail.com>
2026-01-13 20:51:25 +0000

    Fuction 3 done

--- BTP500-LAB1-fuction3-Farhan.py
+++ BTP500-LAB1-fuction3-Farhan.py
@@ -1,9 +1,9 @@
 def function3(list):
- n = len(list)
- for i in range(n - 1):
-  for j in range(n - 1 - i):
-   if list[j] > list[j+1]:
-      tmp = list[j]
-      list[j] = list[j+1]
-      list[j + 1] = tmp
+ n = len(list)                # 1
+ for i in range(n - 1):       # (n - 1) + 1 
+  for j in range(n - 1 - i):  # (n - 1 - i) + 1
+   if list[j] > list[j+1]:    # (n - 1 - i) comparisons
+      tmp = list[j]           # (n - 1 - i) 
+      list[j] = list[j+1]     # (n - 1 - i) 
+      list[j + 1] = tmp       # (n - 1 - i) 
       
